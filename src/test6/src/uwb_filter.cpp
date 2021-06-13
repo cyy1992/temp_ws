24b1beae60768aa2b6ae7ffe5e6453f33b290685
cyy <cyycyag@163.com>
2021-06-13 15:05:07 +0800

    tmp

--- src/test6/src/uwb_filter.cpp
+++ src/test6/src/uwb_filter.cpp
@@ -26,9 +26,9 @@ Kalman::Kalman()
   state_ = Mat(2, 1, CV_32F);
   measurement_ = Mat::zeros(1, 1, CV_32F);
   setIdentity(kf_.measurementMatrix);
-  setIdentity(kf_.processNoiseCov, Scalar::all(0.5));
-  setIdentity(kf_.measurementNoiseCov, Scalar::all(1));
-  setIdentity(kf_.errorCovPost, Scalar::all(1));
+  setIdentity(kf_.processNoiseCov, Scalar::all(1e-5));
+  setIdentity(kf_.measurementNoiseCov, Scalar::all(0.1));
+  setIdentity(kf_.errorCovPost, Scalar::all(1.0));
   
   initialised_ = false;
   
@@ -41,12 +41,15 @@ double Kalman::Var()
     sum += it;
   }
   double mean = sum/distance_queue_.size();
-  double var_sum = 0;
-  for(auto it:distance_queue_)
-  {
-    var_sum += (it - mean) * (it - mean);
-  }
-  return var_sum/distance_queue_.size();
+//   double var_sum = 0;
+//   
+//   for(auto it:distance_queue_)
+//   {
+//     var_sum += (it - mean) * (it - mean);
+//   }
+//   double var = var_sum/distance_queue_.size();
+  double var = (distance_queue_.back() - mean) * (distance_queue_.back() - mean);
+  return var;
 }
 
 double Kalman::Filter(const double& det_time, const double& distance)
@@ -64,7 +67,7 @@ double Kalman::Filter(const double& det_time, const double& distance)
       {
         initialised_ = true;
         state_.at<float>(0) = distance;
-        state_.at<float>(1) = 0;
+        state_.at<float>(1) = 0.0;
         kf_.statePost = state_;
         last_distance_ = distance;
         return distance;
@@ -81,34 +84,35 @@ double Kalman::Filter(const double& det_time, const double& distance)
   }
 //   cout << "1111" <<endl;
   distance_queue_.push_back(distance);
-  if(distance_queue_.size() > 50)
+  if(distance_queue_.size() > 200)
     distance_queue_.erase(distance_queue_.begin());
   double var = Var();
   
 //   cout << var <<endl;
-  if(var > 2.0)
-    return -1;
+  if(var > 1.0)
+    return -2;
 //   if( fabs((last_distance_ - distance)/det_time) > 5.0 || fabs(last_distance_ - distance) > 3.5)
 //     return -1;
   assert(det_time > 0);
-  kf_.transitionMatrix = (Mat_<float>(2, 2) << 1, det_time, 0, 1);
+  kf_.transitionMatrix = (Mat_<float>(2, 2) << 1.0, det_time, 0.0, 1.0);
   measurement_ = distance;
   Mat prediction = kf_.predict();
+  kf_.correct(measurement_);
 //   if(fabs(prediction.at<float>(0) - distance)/det_time > 10.0)
 //   {
 //     return -1;
 //   }
 //   ;
   last_distance_ = distance;
-  return (double)(kf_.correct(measurement_).at<float>(0));
+  return static_cast<double>(kf_.statePost.at<float>(0));
 }
 
 UwbFilter::UwbFilter(const ros::NodeHandle& n):nh_(n)
 {
   uwb_sub_ = nh_.subscribe<nlink_parser::LinktrackNodeframe2>(
-    "/nlink_linktrack_nodeframe2", 5000, &UwbFilter::HandleUwbMsg,this);
+    "/nlink_linktrack_nodeframe2", 1, &UwbFilter::HandleUwbMsg,this);
   filter_uwb_pub_ = nh_.advertise<cartographer_ros_msgs::LinktrackNodeframe2>("/filter_uwb_datas",1);
-  max_distance_ = 120;
+  max_distance_ = 300;
 }
 
 UwbFilter::~UwbFilter()
@@ -147,17 +151,18 @@ void UwbFilter::HandleUwbMsg(const nlink_parser::LinktrackNodeframe2::ConstPtr& 
       kalman_filters_[id].reset(new Kalman);
       unuse_distance_count_[id] = 0;
     }
-    
-    
+//     
+//     
     // 2. kalman filter
     double det_time = (cur_time - last_times_[id]).toSec();
-    cout << id <<", " << det_time << ", " <<distance<<endl;
+//     cout << id <<", " << det_time << ", " <<distance<<endl;
     double filtered_data = kalman_filters_[id]->Filter(det_time, distance);
 //     cout << filtered_data <<endl;
-    if(filtered_data != -1.)
+    
+    
+    if(filtered_data >= 0.)
     {
       last_times_[id] = cur_time;
-      node.dis = filtered_data;
       cartographer_ros_msgs::LinktrackNode2 node2;
       node2.dis = filtered_data;
       node2.id = id;
@@ -172,6 +177,11 @@ void UwbFilter::HandleUwbMsg(const nlink_parser::LinktrackNodeframe2::ConstPtr& 
       
       tmp_point.x = filtered_data;
       filtered_path_pub_[id].publish(tmp_point); 
+    }
+    else if(filtered_data == -2)
+    {
+      unuse_distance_count_[id] +=1;
+      continue;
     }
   }
   if(filtered_uwb_msg.nodes.size() > 0)
